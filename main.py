2024935af463a55c48635e78458a26e19e1433e0
alice-lashuk <alice.lashuk@gmail.com>
2021-04-15 22:13:20 +0200

    task 2

--- main.py
+++ main.py
@@ -6,3 +6,24 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.get("/method")
+def root():
+    return {"method": "GET"}
+
+@app.put("/method")
+def root():
+    return {"method": "PUT"}
+
+@app.post("/method", status_code=201)
+def root():
+    return {"method": "POST"}
+
+@app.delete("/method")
+def root():
+    return {"method": f"DELETE"}
+
+@app.options("/method")
+def root():
+    return {"method": "OPTIONS"}
+
--- tests.py
+++ tests.py
@@ -9,3 +9,28 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
+
+def test_get_method():
+    response = client.get(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+def test_put_method():
+    response = client.put(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+def test_post_method():
+    response = client.post(f"/method")
+    assert response.status_code == 201
+    assert response.json() == {"method": "POST"}
+
+def test_delete_method():
+    response = client.delete(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": f"DELETE"}
+
+def test_options_method():
+    response = client.options(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "OPTIONS"}
